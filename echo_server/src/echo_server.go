4d4fa8e1f79b09402750b343826b483a7e3b1d8b
Vincent Bauchart <bauchart@bvassociates>
2016-03-11 13:00:53 +0100

    initial commit

---
+++ echo_server/src/echo_server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+        "fmt"
+        "net"
+        "bufio"
+        "log"
+        "os"
+)
+
+func echo(s net.Conn, i int, content string) {
+        defer s.Close();
+
+        fmt.Printf("%d: %v <-> %v\n", i, s.LocalAddr(), s.RemoteAddr())
+        b := bufio.NewReader(s)
+        for {
+                line, e := b.ReadBytes('\n')
+                if e != nil || len(line) <= 1 {
+                        break
+                }
+                remote_message := line[0:len(line)-1]
+                message := []byte(fmt.Sprintf("Hello %s, I am %s\n", remote_message, content))
+                s.Write(message)
+        }
+        fmt.Printf("%d: closed\n", i)
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+            log.Fatal(fmt.Sprintf("USAGE: %s /path/to/file", os.Args[0]))
+        }
+
+        file, errfile := os.Open(os.Args[1])
+	if errfile != nil {
+            log.Fatal(errfile)
+        }
+
+	bf := bufio.NewReader(file)
+	content_bytes, isPrefix, errbf := bf.ReadLine()
+        if errbf != nil {
+             log.Fatal(errbf)
+        }
+        if isPrefix {
+        } 
+
+	content := string(content_bytes)
+
+        fmt.Printf("I will answer %s to all requests", content)
+
+        l, e := net.Listen("tcp", ":12321")
+        if e != nil {
+            log.Fatal(e)
+        }
+        fmt.Printf("%v: listenning to 12321\n", l.Addr())
+        for i := 0; e == nil; i++ {
+                var s net.Conn
+                s, e = l.Accept()
+                go echo(s, i, content)
+        }
+}
